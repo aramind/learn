31748397d22fb886f83909b1f841538a88c9ce93
aramind <88814622+aramind@users.noreply.github.com>
2025-06-20 21:47:46 +0800

    feat: initial setup

--- NEXTJS/codevolution/src/app/page.tsx
+++ NEXTJS/codevolution/src/app/page.tsx
@@ -16,11 +16,13 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
+              src/app/page.tsx
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">by Robin Mon Miranda.</li>
+          <li className="tracking-[-.01em]">
+            Save and see your changes instantly.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
