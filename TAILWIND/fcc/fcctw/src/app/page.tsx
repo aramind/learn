82bcf7698d63a3ed86ec2e0754d8762b6e8f9c6c
aramind <88814622+aramind@users.noreply.github.com>
2025-06-24 21:52:53 +0800

    feat: learning typography styling

---
+++ TAILWIND/fcc/fcctw/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold border-4 border-mygreen-100 text-mygreen-200">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl text-mygreen-200">Hello world!</h1>
+    </>
+  );
+}
---
+++ TAILWIND/fcc/fcctw/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mygreen: {
+          100: "#49e659",
+          200: "#42e652",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
