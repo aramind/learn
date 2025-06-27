0a36e90b97b0165e9466d92083fdd900bcfc2489
aramind <88814622+aramind@users.noreply.github.com>
2025-06-27 23:05:45 +0800

    feat: learning sequential and parallel data fetching

---
+++ NEXTJS/data-fetching-demo/src/app/loading.tsx
@@ -0,0 +1,10 @@
+export default async function LoadingPage() {
+  return (
+    // <div className="flex items-center justify-center h-screen">
+    //   <div className="animate-spin rounded-full h-32 w-32 border-t-2">
+    //     Loading
+    //   </div>
+    // </div>
+    <div>LOADING PAGE IN HOME...</div>
+  );
+}
---
+++ NEXTJS/data-fetching-demo/src/app/users-server/loading.tsx
@@ -0,0 +1,8 @@
+export default function LoadingPage() {
+  console.log("Loading USERS page..."); // Should appear in terminal
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-yellow-200 text-black text-3xl font-bold">
+      LOADING USERS PAGE...
+    </div>
+  );
+}
