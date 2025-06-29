41b31cd969f48bdc5c6c99b9cd1eebc43df50307
aramind <88814622+aramind@users.noreply.github.com>
2025-06-29 22:05:36 +0800

    feat: using Form component in forms

--- NEXTJS/data-fetching-demo/src/app/page.tsx
+++ NEXTJS/data-fetching-demo/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Search } from "@/components/search";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Link href={"/users-server"}>USERS</Link>
+      <Search />
     </div>
   );
 }
